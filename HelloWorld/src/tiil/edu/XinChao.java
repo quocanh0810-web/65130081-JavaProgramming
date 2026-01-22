e43c3638851cc5e6b72898108166ff003bbbac50
Quoc Anh <quocanh0810mwg@gmail.com>
2026-01-22 18:22:08 +0700

    Vi Du
    Demo

---
+++ HelloWorld/src/tiil/edu/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Toi lan dau hoc Java, va dang thu nghiem quan li ma nguon voi GIT");
+
+	}
+
+}

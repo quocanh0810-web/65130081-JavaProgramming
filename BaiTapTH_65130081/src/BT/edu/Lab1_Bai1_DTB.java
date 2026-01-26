3a41ff12eb3a0c931e4ba82d76575437a6cfcd61
Quoc Anh <quocanh0810mwg@gmail.com>
2026-01-26 22:26:53 +0700

    Cap Nhat

--- BaiTapTH_65130081/src/BT/edu/Lab1_Bai1_DTB.java
+++ BaiTapTH_65130081/src/BT/edu/Lab1_Bai1_DTB.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package BT.edu;
 
 import java.util.Scanner;
@@ -9,13 +7,13 @@ public class Lab1_Bai1_DTB {
 	public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Nhập họ và tên sinh viên: ");
+        System.out.print("Nhap ho va ten sinh vien: ");
         String hoTen = sc.nextLine();
 
-        System.out.print("Nhập điểm trung bình: ");
+        System.out.print("Nhap diem trung binh: ");
         double dtb = sc.nextDouble();
 
-        System.out.println("Họ tên sinh viên: " + hoTen);
-        System.out.println("Điểm trung bình: " + dtb);
+        System.out.println("Ho ten sinh vien: " + hoTen);
+        System.out.println("Diem trung binh: " + dtb);
     }
 }
